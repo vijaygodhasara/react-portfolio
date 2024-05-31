95f1e0abe33d3c85429c319b029feac65afcc7ce
Tarun Meel <58963927+Tarunmeel94@users.noreply.github.com>
2024-05-31 10:14:42 +0530

    updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -32,10 +32,7 @@ class Portfolio extends Component {
           <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
 
-            <div
-              id="portfolio-wrapper"
-              className="bgrid-quarters s-bgrid-thirds cf"
-            >
+            <div id="portfolio-wrapper" className=" s-bgrid-thirds cf">
               {projects}
             </div>
           </div>
